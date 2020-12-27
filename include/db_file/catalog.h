3007422c7ca0e260a33c166c50ae9b7dc4e200ef
Laura Bin <l.binacchi@gmail.com>
2020-12-27 17:09:40 +0100

    fields length constants and fillers

--- include/db_file/catalog.h
+++ include/db_file/catalog.h
@@ -27,8 +27,37 @@
 #define N_RES_CMP 100000
 #define N_RES_PRS 500000
 
-/* Size of tuple prefix corresponding to the table type (CTR, JOB, etc.) */
-#define PREF_LEN 8
+/* Fields length */
+#define PREFIX_LEN              8
+#define COUNTRY_NAME_LEN        16
+#define COUNTRY_ZONE_LEN        16
+#define COUNTRY_ISO_LEN         4
+
+#define JOB_LEVEL_LEN           22
+#define JOB_DEPARTMENT_LEN      24
+#define JOB_NAME_LEN            38
+
+#define INDUSTRY_SECTOR_LEN     20
+#define INDUSTRY_NAME_LEN       32
+
+#define GROUP_NAME_LEN          32
+
+#define COMPANY_NAME_LEN        62
+#define COMPANY_ADDRESS_LEN     60
+#define COMPANY_ZIP_CODE_LEN    12
+#define COMPANY_CITY_LEN        32
+#define COMPANY_PHONE_LEN       20
+#define COMPANY_WEBSITE_LEN     38
+#define COMPANY_CREATION_LEN    12
+
+#define PERSON_TITLE_LEN        12
+#define PERSON_FIRSTNAME_LEN    30
+#define PERSON_LASTNAME_LEN     38
+#define PERSON_GENDER_LEN       2
+#define PERSON_CREATION_LEN     12
+#define PERSON_PHONE_LEN        14
+#define PERSON_MOBILE_LEN       14
+#define PERSON_EMAIL_LEN        58
 
 /* Types of tables & table count */
 enum table {
@@ -41,10 +70,17 @@ enum table {
     TAB_COUNT   // number of tables in the database
 };
 
+/* Indexes & count */
+enum index {
+    PERS_BY_COMP_ID,
+    PERS_BY_NAME,
+    I_COUNT     // number of indexes in the database
+};
+
 /* Table metadata */
 struct db;
 struct table_metadata {
-    char prefix[PREF_LEN];  // tuple prefix in the database
+    char prefix[PREFIX_LEN];  // tuple prefix in the database
     char display_name[32];  // name displayed to the user
     unsigned n_reserved;    // number of tuples reserved in the table
     size_t size;            // tuple size
@@ -63,69 +99,85 @@ extern const struct table_metadata tables_metadata[TAB_COUNT];
 
 /* Country tuple */
 struct country {
-    char type[PREF_LEN];    // "CTR"
+    char type[PREFIX_LEN];  // "CTR"
     unsigned id;            // pk
-    char name[24];
-    char zone[24];
-    char iso[4];
+    char name[COUNTRY_NAME_LEN];
+    char zone[COUNTRY_ZONE_LEN];
+    char iso[COUNTRY_ISO_LEN];
+    char filler[16];
 };
 
 /* Job tuple */
 struct job {
-    char type[PREF_LEN];    // "JOB"
+    char type[PREFIX_LEN];  // "JOB"
     unsigned id;            // pk
-    char level[22];
-    char department[24];
-    char name[38];
+    char level[JOB_LEVEL_LEN];
+    char department[JOB_DEPARTMENT_LEN];
+    char name[JOB_NAME_LEN];
 };
 
 /* Industry tuple */
 struct industry {
-    char type[PREF_LEN];    // "IND"
+    char type[PREFIX_LEN];  // "IND"
     unsigned id;            // pk
-    char sector[20];
-    char name[32];
+    char sector[INDUSTRY_SECTOR_LEN];
+    char name[INDUSTRY_NAME_LEN];
 };
 
 /* Group tuple */
 struct group {
-    char type[PREF_LEN];    // "GRP"
+    char type[PREFIX_LEN];  // "GRP"
     unsigned id;            // pk
-    char name[46];
+    char name[GROUP_NAME_LEN];
     unsigned country_id;    // fk
+    char filler[16];
 };
 
 /* Company tuple */
 struct company {
-    char type[PREF_LEN];    // "CMP"
+    char type[PREFIX_LEN];  // "CMP"
     unsigned id;            // pk
     unsigned id_group;      // fk
     unsigned id_country;    // fk
     unsigned id_industry;   // fk
-    char name[64];
-    char address[64];
-    char zip_code[16];
-    char city[32];
-    char phone_number[24];
-    char website[42];
-    char creation_date[12];
+    char name[COMPANY_NAME_LEN];
+    char address[COMPANY_ADDRESS_LEN];
+    char zip_code[COMPANY_ZIP_CODE_LEN];
+    char city[COMPANY_CITY_LEN];
+    char phone_number[COMPANY_PHONE_LEN];
+    char website[COMPANY_WEBSITE_LEN];
+    char creation_date[COMPANY_CREATION_LEN];
     unsigned n_employees;
     float share_value;
+    char filler[20];
 };
 
 /* Person tuple */
 struct person {
-    char type[PREF_LEN];    // "PRS"
+    char type[PREFIX_LEN];  // "PRS"
     unsigned id;            // pk
     unsigned id_company;    // fk
     unsigned id_job;        // fk
-    char title[16];
-    char firstname[32];
-    char lastname[42];
-    char gender[2];
-    char creation_date[12]; //struct tm creation_date; // date of creation of the record
-    char phone_number[16];
-    char mobile_number[16];
-    char email[64];
+    char title[PERSON_TITLE_LEN];
+    char firstname[PERSON_FIRSTNAME_LEN];
+    char lastname[PERSON_LASTNAME_LEN];
+    char gender[PERSON_GENDER_LEN];
+    char creation_date[PERSON_CREATION_LEN];
+    char phone_number[PERSON_PHONE_LEN];
+    char mobile_number[PERSON_MOBILE_LEN];
+    char email[PERSON_EMAIL_LEN];
     unsigned shares;
+    char filler[20];
+};
+
+/* Person by company id index */
+struct person_by_company {
+    unsigned company_id;
+    unsigned offset;
+};
+
+/* Person by name index */
+struct person_by_name {
+    char lastname[PERSON_LASTNAME_LEN];
+    unsigned offset;
 };
