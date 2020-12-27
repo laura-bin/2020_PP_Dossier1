bf5862389062cda3851cb3f317754c5ab7034e93
Laura Bin <l.binacchi@gmail.com>
2020-12-27 18:51:48 +0100

    use length constants

--- src/table/group.c
+++ src/table/group.c
@@ -15,6 +15,7 @@
 #include <string.h>
 
 #include "table/group.h"
+#include "utils/preprocess_string.h"
 #include "utils/string_comparison.h"
 
 int import_group(struct db *db, char *csv_line) {
@@ -26,7 +27,7 @@ int import_group(struct db *db, char *csv_line) {
     memset(&new_rec, 0, sizeof(struct group));
 
     // set type
-    strncpy(new_rec.type, tables_metadata[COUNTRY].prefix, PREF_LEN);
+    strncpy(new_rec.type, tables_metadata[COUNTRY].prefix, PREFIX_LEN);
 
     // set id
     tok = strtok(csv_line, ";");
@@ -50,13 +51,15 @@ int import_group(struct db *db, char *csv_line) {
 }
 
 int export_group(struct db *db) {
-    struct group tuple;       // tuple read from the database file
+    struct group tuple;         // tuple read from the database file
     char new_line[CSV_BUF_LEN]; // new line to write in the csv file
 
     memset(&tuple, 0, sizeof(struct group));
     if (fread(&tuple, sizeof(struct group), 1, db->dat_file) == 1) {
         sprintf(new_line, "%d;%s;%d\n", tuple.id, tuple.name, tuple.country_id);
-        if (strlen(new_line) == (long unsigned)fprintf(db->csv_file, new_line)) return 1;
+        if (strlen(new_line) == (long unsigned)fprintf(db->csv_file, new_line)) {
+            return 1;
+        }
     }
     return 0;
 }
@@ -78,17 +81,21 @@ int load_groups(struct db *db, int count) {
 }
 
 void print_group(struct group *group) {
-    printf("%6d %-48s %8d\n",
+    printf("%" STR(ID_LEN) "d "
+            "%-" STR(GROUP_NAME_LEN) "s "
+            "%" STR(ID_LEN) "d\n",
             group->id,
             group->name,
             group->country_id);
 }
 
 void print_group_header(void) {
-    printf("%6s %-48s %-8s\n",
+    printf("%" STR(ID_LEN) "s "
+            "%-" STR(GROUP_NAME_LEN) "s "
+            "%" STR(ID_LEN) "s\n",
             "ID",
             "NAME",
-            "COUNTRY");
+            "COUNTRY ID");
 }
 
 void *compare_group(struct db *db, unsigned i, char *searched) {
